b21f91c69a38f0572ef2b46ee5861f635f1be30e
avivashishta29 <avivashishta@gmail.com>
2022-04-22 23:12:07 +0530

    blog component

---
+++ components/BlogComponent/index.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+
+function BlogComponent() {
+  const [flag, setFlag] = useState(false);
+  var cursor;
+
+  useEffect(() => {
+    if (flag) {
+      cursor = document.getElementById("myBlogImg");
+      var cursor2 = document.getElementById("cursor");
+      document.body.addEventListener("mousemove", function (e) {
+        (cursor.style.left = e.clientX + "px"),
+          (cursor.style.display = "block"),
+          cursor.classList.remove("show"),
+          cursor.classList.add("show"),
+          cursor2.classList.remove("cursor"),
+          (cursor.style.top = e.clientY + "px");
+      });
+    } else {
+      var cursor2 = document.getElementById("cursor");
+      cursor = document.getElementById("myBlogImg");
+      document.body.addEventListener("mousemove", function (e) {
+        (cursor.style.left = e.clientX + "px"),
+          (cursor.style.display = "none"),
+          cursor.classList.remove("show"),
+          cursor2.classList.add("cursor"),
+          (cursor.style.top = e.clientY + "px");
+      });
+    }
+  }, [flag]);
+
+  return (
+    <div
+      className={styles.teamContainer}
+      style={{ zIndex: 100 }}
+      onMouseEnter={() => {
+        setFlag(true);
+      }}
+      onMouseLeave={() => {
+        setFlag(false);
+      }}
+    >
+      <div className={styles.leftContainer}>
+        <img
+          className="myBlogImg"
+          id="myBlogImg"
+          src={
+            "https://www.giantfreakinrobot.com/wp-content/uploads/2021/05/tony-stark-robert-downey-jr.jpg"
+          }
+          style={{ display: !flag && "none" }}
+        />
+        <p className={styles.list}>01</p>
+        <p className={styles.headingName}>NAME 1</p>
+      </div>
+    </div>
+  );
+}
+
+export default BlogComponent;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import BlogComponent from "../components/BlogComponent";
 export default function Home() {
   return (
     <div>
       <Header />
+      <BlogComponent />
+      <BlogComponent />
+      <BlogComponent />
+      <BlogComponent />
     </div>
   );
 }
